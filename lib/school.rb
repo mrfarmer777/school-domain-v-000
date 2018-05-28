12944d732d0c77f2c41f57c93c47f7b19a471d9d
Matt Farmer <mrfarmer777@gmail.com>
2018-05-28 21:24:00 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+  
+  def initialize(name)
+    @name=name
+    @roster={}
+    
+  end
+
+  def add_student(student,gr)
+    if !@roster[gr]
+      @roster[gr]=[]
+    end
+    @roster[gr]<<student
+  end
+
+  def grade(gr)
+    @roster[gr]
+  end
+
+  def sort
+    stus=[]
+    @roster.each do |gr|
+      gr.each do |stu|
+        stus<<stu
+      end
+    end
+    stus.sort!
+    return stus
+end
+    
